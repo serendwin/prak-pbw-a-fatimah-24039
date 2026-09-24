0c9d831365a2319e82b3df5c473ae464e1c8c532
serendwin <twentyhours133@gmail.com>
2026-09-25 05:18:37 +0700

    tugas prak1

--- praktikum1/biodata.php
+++ praktikum1/biodata.php
@@ -6,14 +6,38 @@ function statuskelulusan(float $ipk): string
     return 'Perlu peningkatan';
 }
 
+function countingipk(array $ips): float
+{
+    return array_sum($ips)/count($ips);
+}  
+
+function statusmahasiswa(int $semester): string
+{
+    if ($semester == 8) 
+    {
+        return 'lulus';
+    }
+    return 'aktif'; 
+}
+
 $mahasiswa =
 [
     'nim' => '66666',
-    'nama' => 'Sepuki cihuy',
+    'nama' => 'Merugame udon',
     'prodi' => 'Teknik Informatika',
-    'semester' => 1,
-    'ipk' => 9.99,
+    'semester' => 
+    [
+        1 => 3.8,
+        2 => 3.9,
+        3 => 3.9,
+        4 => 3.9,
+        5 => 4.0,                       
+    ]
 ];
+
+$ipk = countingipk($mahasiswa['semester']);
+
+$semesterTerakhir = max(array_keys($mahasiswa['semester']));
 ?>
 
 <!DOCTYPE html>
@@ -21,17 +45,25 @@ $mahasiswa =
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Biodata</title>
+    <title>Biodata Mahasiswa</title>
 </head>
+
 <body>
-    <h1>Biodata Mahasiswa</h1>
+    <h4>Biodata Mahasiswa</h4>
     <ul>
-        <?php foreach ($mahasiswa as $kunci => $nilai): ?>
-        <li>
-            <?= ucfirst($kunci) ?>: <?= htmlspecialchars((string)$nilai) ?>
-        </li>
-        <?php endforeach; ?>
+        <li>NIM : <?= htmlspecialchars($mahasiswa['nim']) ?></li>
+        <li>Nama : <?= htmlspecialchars($mahasiswa['nama']) ?></li>
+        <li>Prodi : <?= htmlspecialchars($mahasiswa['prodi']) ?></li>
+        <li>Status : <?= statusmahasiswa($semesterTerakhir) ?></li>
     </ul>
-    <p>Prediket: <?= statuskelulusan($mahasiswa['ipk']) ?></p>
+
+    <h4>Nilai IPS</h4>
+    <ul>
+        <?php foreach ($mahasiswa['semester'] as $semester => $ips): ?>
+            <li>Semester <?= $semester ?> : IPS <?= number_format($ips, 2) ?></li>
+        <?php endforeach; ?>
+        <div>IPK : <?= number_format($ipk, 2) ?></div>
+        <div>Predikat : <?= statuskelulusan($ipk) ?></div>        
+    </ul>
 </body>
 </html>
--- praktikum1/kalkulator.php
+++ praktikum1/kalkulator.php
@@ -1,66 +1,82 @@
 <?php
 
-$hasil = null;
+$nama_barang = '';
+$harga = 0;
+$jumlah = 0;
+$subtotal = 0;
 $pesan = '';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
-    {
-        $a = (float) ($_POST['a'] ?? 0);
-        $b = (float) ($_POST['b'] ?? 0);
-        $operator = $_POST['operator'] ?? '+';
-        
-        switch ($operator)
-        {
-            case '+':
-                $hasil = $a + $b;
-                break;
-            case '-':
-                $hasil = $a - $b;
-                break;
-            case '*':
-                $hasil = $a * $b;
-                break;
-            case '/':
-                if ($b == 0)
-                    {
-                        $pesan = 'Pembagian dengan nol tidak diperbolehkan.';
-                    }
-                else
-                    {
-                        $hasil = $a/$b;
-                    }
-                break;
-                default:
-                    $pesan = 'Operator tidak valid';
-        }
+{
+    $nama_barang = $_POST['nama_barang'] ?? '';
+    $harga = (float) ($_POST['harga'] ?? 0);
+    $jumlah = (int) ($_POST['jumlah'] ?? 0);
 
+    if ($harga <= 0)
+    {
+        $pesan = 'Harga barang harus lebih dari 0.';
     }
+    elseif ($jumlah <= 0)
+    {
+        $pesan = 'Jumlah barang harus lebih dari 0.';
+    }
+    elseif ($nama_barang == '')
+    {
+        $pesan = 'Nama barang harus diisi.';
+    }
+    else
+    {
+        $subtotal = $harga * $jumlah;
+    }
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kalkulator</title>
+    <title>Struk Pembelian</title>
 </head>
 <body>
-    <h1>Kalkulator sederhana</h1>
+    <h1>Struk Pembelian</h1>
     <form method="post">
-        <input type="number" step="any" name="a" required>
-        <select nama="operator">
-            <option>+</option>
-            <option>-</option>
-            <option>*</option>
-            <option>/</option>
-        </select>
-        <input type="number" step="any" name="b" required>
-        <button type="submit">Hitung</button>
+        <label>Nama Barang:</label><br>
+        <input type="text" name="nama_barang" required>
+        <br><br>
+        <label>Harga Barang:</label><br>
+        <input type="text" name="harga" min="1" required>
+        <br><br>
+        <label>Jumlah:</label><br>
+        <input type="number" name="jumlah" min="1" required>
+        <br><br>
+        <button type="submit">Hitung Total</button>
     </form>
+
     <?php if ($pesan): ?>
         <p><?= htmlspecialchars($pesan) ?></p>
-    <?php elseif ($hasil !== null): ?>
-        <p>Hasil: <?= htmlspecialchars((string)$hasil) ?></p>
+    <?php elseif ($subtotal > 0): ?>
+        <h2>Detail Pembelian</h2>
+        <p>
+            Nama Barang:
+            <?= htmlspecialchars($nama_barang) ?>
+        </p>
+        <p>
+            Harga:
+            Rp <?= number_format($harga, 0, ',', '.') ?>
+        </p>
+        <p>
+            Jumlah:
+            <?= $jumlah ?>
+        </p>
+        <p>
+            Subtotal:
+            Rp <?= number_format($subtotal, 0, ',', '.') ?>
+        </p>
+       <h3>
+            Total:
+            Rp <?= number_format($subtotal, 0, ',', '.') ?>
+        </h3>
     <?php endif; ?>
 </body>
 </html>
